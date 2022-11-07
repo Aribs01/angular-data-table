dbd5e22066729e7a553775300c898867e2c6a7ee
aribs01 <abiolaaribisala@gmail.com>
2022-11-07 13:37:25 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppMainComponent } from './app.main.component';
+import { TopbarComponent } from './components/topbar/topbar.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppMainComponent,
+    TopbarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
