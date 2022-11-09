7dceecacc80485a3d40e3b3dd229a2ba548f82c5
aribs01 <abiolaaribisala@gmail.com>
2022-11-09 12:46:16 +0100

    http calls

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,6 +1,8 @@
-import { Injectable } from '@angular/core';
+import { Injectable, isDevMode } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { User } from '../schemas/User';
+import { environment } from 'src/environments/environment';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -10,8 +12,14 @@ export class UserService {
   constructor(private http: HttpClient) { }
 
   getAllUser() {
-    return this.http.get<any>('assets/data.json')
-    .toPromise()
-    .then(res => <User[]>res.data).then(data => { return data; });
+    return this.http.get<any>(this.getBaseApiUrl()+'users/70R0l0YsR7OV667')
+  }
+
+  markPaid(data: any): Observable<any> {
+    return this.http.patch<any>(this.getBaseApiUrl() + 'mark-paid/' + data.id, data);
+  }
+
+  private getBaseApiUrl(): string {
+    return isDevMode() ? environment.apiBaseUrl : environment.apiBaseUrl;
   }
 }
